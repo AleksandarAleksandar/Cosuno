e9605b39c73ba596e7b015aee0b03d55951ec476
Aleksandar Vasiljevic <aleksandarvasiljevic111@gmail.com>
2022-06-22 22:40:16 +0200

    add dotenv back

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express');
+const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 
 const connectDB = require('./config/db');
